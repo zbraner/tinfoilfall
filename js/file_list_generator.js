f47e9cfc56f393e2e0639eaee37dd49ebbb3d258
zbraner <zacharybraner@gmail.com>
2023-06-26 11:07:40 -0400

    had an idea for how to fix the filepath--use the correct filepathgit add js/file_list_generator.js

--- js/file_list_generator.js
+++ js/file_list_generator.js
@@ -1,4 +1,4 @@
-import { glob, globSync, globStream, globStreamSync, Glob } from 'glob'
+import { glob, globSync, globStream, globStreamSync, Glob } from '../node_modules/glob'
 
 console.log("Finished importing glob...")
 // or using commonjs, that's fine, too
